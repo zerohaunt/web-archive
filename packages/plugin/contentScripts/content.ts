ef4ea62de70f444c574cb7c522d430702fb72bca
banzhe <banzhe5@outlook.com>
2024-10-23 20:14:25 +0800

    feat: add modal in content script

--- packages/plugin/contentScripts/content.ts
+++ packages/plugin/contentScripts/content.ts
@@ -1,13 +1,43 @@
 import { onMessage, sendMessage } from 'webext-bridge/content-script'
 import { getCurrentPageData } from '~/utils/singleFile'
 
+function createModal() {
+  const modal = document.createElement('div')
+  modal.innerHTML = `
+    <div>
+      Scraping Page Data...
+      <br />
+      <span></span>
+    </div>
+  `
+  Object.assign(modal.style, {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    background: 'rgba(0, 0, 0, 0.5)',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 99999999,
+  })
+  return modal
+}
+
 onMessage('scrape-page-data', async ({ data: singleFileSetting }) => {
-  return await getCurrentPageData({
+  const modal = createModal()
+  document.body.appendChild(modal)
+
+  const pageData = await getCurrentPageData({
     ...singleFileSetting,
     onprogress: (data) => {
-      sendMessage('scrape-page-progress', { stage: data.type }, 'popup')
     },
   })
+
+  modal.remove()
+  return pageData
 })
 
 onMessage('get-basic-page-data', async () => {
