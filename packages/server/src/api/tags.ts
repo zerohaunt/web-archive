b23d12e1b7d0989c3ba1f5843ddafd84f27add42
banzhe <banzhe5@outlook.com>
2024-10-29 18:29:04 +0800

    feat: add update tag api

--- packages/server/src/api/tags.ts
+++ packages/server/src/api/tags.ts
@@ -1,8 +1,8 @@
-import { isNil } from '@web-archive/shared/utils'
+import { isNil, isNumberString } from '@web-archive/shared/utils'
 import { Hono } from 'hono'
 import { validator } from 'hono/validator'
 import type { HonoTypeUserInformation } from '~/constants/binding'
-import { insertTag, selectAllTags } from '~/model/tag'
+import { insertTag, selectAllTags, updateTag } from '~/model/tag'
 import result from '~/utils/result'
 
 const app = new Hono<HonoTypeUserInformation>()
@@ -19,18 +19,48 @@ app.post(
     if (isNil(value.name) || typeof value.name !== 'string') {
       return c.json(result.error(400, 'Name is required'))
     }
+    // todo check color type?
     return {
       name: value.name as string,
+      color: value.color,
     }
   }),
   async (c) => {
-    const { name } = c.req.valid('json')
+    const { name, color = '#ffffff' } = c.req.valid('json')
 
-    if (await insertTag(c.env.DB, { name })) {
+    if (await insertTag(c.env.DB, { name, color })) {
       return c.json(result.success(true))
     }
 
     return c.json(result.error(500, 'Failed to create tag'))
+  },
+)
+
+app.post(
+  '/update',
+  validator('json', (value, c) => {
+    if (isNil(value.id) || !isNumberString(value.id)) {
+      return c.json(result.error(400, 'ID is required'))
+    }
+    if (isNil(value.name) && isNil(value.color) && isNil(value.pageIds)) {
+      return c.json(result.error(400, 'Name, color, or pageIds is required'))
+    }
+
+    return {
+      id: Number(value.id),
+      name: value.name,
+      color: value.color,
+      pageIds: value.pageIds as Array<number> | undefined,
+    }
+  }),
+  async (c) => {
+    const { id, name, color, pageIds } = c.req.valid('json')
+
+    if (await updateTag(c.env.DB, { id, name, color, pageIds })) {
+      return c.json(result.success(true))
+    }
+
+    return c.json(result.error(500, 'Failed to update tag'))
   },
 )
 
--- packages/server/src/model/tag.ts
+++ packages/server/src/model/tag.ts
@@ -1,3 +1,4 @@
+import { isNil } from '@web-archive/shared/utils'
 import type { Tag } from '~/sql/types'
 
 async function selectAllTags(DB: D1Database) {
@@ -19,16 +20,61 @@ async function selectAllTags(DB: D1Database) {
   return tagList
 }
 
-async function insertTag(DB: D1Database, options: { name: string }) {
+async function getTagById(DB: D1Database, id: number) {
+  const sql = `
+    SELECT 
+      *
+    FROM tags
+    WHERE id = ?
+  `
+  const tag = await DB.prepare(sql).bind(id).first<Tag>()
+  return tag
+}
+
+async function insertTag(DB: D1Database, options: { name: string, color: string }) {
+  const { name, color } = options
   const sql = `
     INSERT INTO tags (name, color) 
-    VALUES (?, '#ffffff')
+    VALUES (?, ?)
   `
-  const sqlResult = await DB.prepare(sql).bind(options.name).run()
+  const sqlResult = await DB.prepare(sql).bind(name, color).run()
+  return sqlResult.success
+}
+
+async function updateTag(DB: D1Database, options: { id: number, name?: string, color?: string, pageIds?: Array<number> }) {
+  const { id, name, color, pageIds } = options
+  if (isNil(id)) {
+    throw new Error('Tag id is required')
+  }
+  if (isNil(name) && isNil(color) && isNil(pageIds)) {
+    throw new Error('At least one field is required')
+  }
+  let sql = `
+    UPDATE tags
+    SET 
+  `
+  const bindParams: (number | string)[] = []
+  if (name) {
+    sql += `name = ?, `
+    bindParams.push(name)
+  }
+  if (color) {
+    sql += `color = ?, `
+    bindParams.push(color)
+  }
+  if (pageIds) {
+    sql += `pageIds = ?, `
+    bindParams.push(JSON.stringify(pageIds))
+  }
+  sql = `${sql.slice(0, -2)} WHERE id = ?`
+  bindParams.push(id)
+  const sqlResult = await DB.prepare(sql).bind(...bindParams).run()
   return sqlResult.success
 }
 
 export {
   selectAllTags,
   insertTag,
+  getTagById,
+  updateTag,
 }
