3a39c1c593b7cdfacd3f77623236ded393b261f1
Ray-D-Song <songraysmail@gmail.com>
2024-10-31 17:08:04 +0800

    refactor: sidebar
    new sidebar view

--- packages/web/src/components/hamburger.tsx
+++ packages/web/src/components/hamburger.tsx
@@ -1,3 +1,4 @@
+import { useSidebar } from '@web-archive/shared/components/side-bar'
 import { SquareMenu } from 'lucide-react'
 
 interface HamburgerProps {
@@ -6,8 +7,9 @@ interface HamburgerProps {
 }
 
 function Hamburger({ className, onClick }: HamburgerProps) {
+  const { openMobile, setOpenMobile } = useSidebar()
   return (
-    <div className={`text-white bg-blue-600 py-2 pr-3 pl-1 rounded-r-[50%] ${className}`} onClick={onClick}>
+    <div className={`text-white bg-blue-600 py-2 pr-3 pl-1 rounded-r-[50%] ${className}`} onClick={() => setOpenMobile(!openMobile)}>
       <SquareMenu className="h-5 w-5" />
     </div>
   )
