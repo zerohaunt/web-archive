27e633b6759c46899062593fbbcf01eefc75a198
Ray <songraysmail@gmail.com>
2024-11-09 17:51:59 +0800

    fix: read mode overflow

--- packages/web/src/components/readability-page-content.tsx
+++ packages/web/src/components/readability-page-content.tsx
@@ -19,10 +19,10 @@ function ReadabilityPageContent({ pageHtml }: ReadabilityPageContentProps) {
   }, [article?.content])
 
   return (
-    <main className="flex flex-col gap-4 items-center">
+    <main className="flex flex-col gap-4 items-center w-full h-full">
       <h1 className="text-2xl font-bold">{article?.title}</h1>
       <h3 className="text-sm text-muted-foreground max-w-prose">{article?.excerpt}</h3>
-      <div className="prose dark:prose-invert" ref={articleContainerRef}></div>
+      <div className="prose dark:prose-invert w-full" ref={articleContainerRef}></div>
     </main>
   )
 }
